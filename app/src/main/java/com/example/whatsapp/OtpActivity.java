1f9cdcd04c6a3ffd913797710f17ce90271d6617
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-12-10 11:03:03 +0600

    otp finished

--- app/src/main/java/com/example/whatsapp/OtpActivity.java
+++ app/src/main/java/com/example/whatsapp/OtpActivity.java
@@ -1,14 +1,95 @@
 package com.example.whatsapp;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.whatsapp.databinding.ActivityOtpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.mukesh.OnOtpCompletionListener;
+
+import java.util.concurrent.TimeUnit;
 
 public class OtpActivity extends AppCompatActivity {
+
+    ActivityOtpBinding binding;
+    FirebaseAuth auth;
+    String verificationId;
+    ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otp);
+        binding = ActivityOtpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Sending otp code");
+        dialog.setCancelable(false);
+        dialog.show();
+
+        getSupportActionBar().hide();
+        binding.otpView.requestFocus();
+
+        auth = FirebaseAuth.getInstance();
+
+        String phone_number = getIntent().getStringExtra("phone_number");
+
+        binding.otptextView.setText("Verify " + phone_number);
+
+        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber(phone_number)
+                .setTimeout(120l, TimeUnit.SECONDS)
+                .setActivity(OtpActivity.this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(s, forceResendingToken);
+                        dialog.dismiss();
+                        verificationId = s;
+                    }
+                })
+                .build();
+
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
+        binding.otpView.setOtpCompletionListener(new OnOtpCompletionListener() {
+            @Override
+            public void onOtpCompleted(String otp) {
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, otp);
+
+                auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(OtpActivity.this, "Yes", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(OtpActivity.this, "No", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/whatsapp/PhoneNumberActivity.java
+++ app/src/main/java/com/example/whatsapp/PhoneNumberActivity.java
@@ -1,17 +1,34 @@
 package com.example.whatsapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.whatsapp.databinding.ActivityPhoneNumberBinding;
 
 public class PhoneNumberActivity extends AppCompatActivity {
+
+    ActivityPhoneNumberBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number);
+        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().hide();
+        binding.phoneNumber.requestFocus();
 
+        binding.phoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PhoneNumberActivity.this, OtpActivity.class);
+                intent.putExtra("phone_number", binding.phoneNumber.getText().toString());
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
