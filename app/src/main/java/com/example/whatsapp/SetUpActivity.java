2846968182dc320844c47ae3a376019e7a81705b
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-12-10 21:59:43 +0600

    SetUpActivity.java finished

--- app/src/main/java/com/example/whatsapp/SetUpActivity.java
+++ app/src/main/java/com/example/whatsapp/SetUpActivity.java
@@ -1,16 +1,24 @@
 package com.example.whatsapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.whatsapp.databinding.ActivitySetUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class SetUpActivity extends AppCompatActivity {
 
@@ -18,12 +26,19 @@ public class SetUpActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseDatabase database;
     FirebaseStorage storage;
+    Uri selectedImg;
+    ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySetUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        getActionBar().hide();
+
+        dialog.setMessage("Updating profile");
+        dialog.setCancelable(false);
 
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
@@ -39,6 +54,70 @@ public class SetUpActivity extends AppCompatActivity {
             }
         });
 
+        binding.setBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = binding.setName.getText().toString();
+                if (name.isEmpty()){
+                    binding.setName.setError("Please enter your name!");
+                    return;
+                }
+
+                dialog.show();
+                if (selectedImg != null){
+                    StorageReference reference = storage.getReference().child("Profiles").child(auth.getUid());
+                    reference.putFile(selectedImg).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if (task.isSuccessful()){
+                                reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        String imageUrl = uri.toString();
+
+                                        String uid = auth.getUid();
+                                        String phone = auth.getCurrentUser().getPhoneNumber();
+                                        String name = binding.setName.getText().toString();
+
+                                        Users users = new Users(uid,name, phone, imageUrl);
+
+                                        database.getReference().child("users")
+                                                .child(uid)
+                                        .setValue(users)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                 dialog.dismiss();
+                                                startActivity(new Intent(SetUpActivity.this, MainActivity.class));
+                                                finish();
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        }
+                    });
+                } else {
+                    String uid = auth.getUid();
+                    String phone = auth.getCurrentUser().getPhoneNumber();
+
+                    Users users = new Users(uid,name, phone, "no image");
+
+                    database.getReference().child("users")
+                            .child(uid)
+                            .setValue(users)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    dialog.dismiss();
+                                    startActivity(new Intent(SetUpActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                            });
+                }
+            }
+        });
+
     }
 
     @Override
@@ -48,6 +127,7 @@ public class SetUpActivity extends AppCompatActivity {
         if (data != null){
             if (data.getData() != null){
                 binding.setImage.setImageURI(data.getData());
+                selectedImg = data.getData();
             }
         }
 
